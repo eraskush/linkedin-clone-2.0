bc1523347fa8a01d9a8728c6999375293ef1fa05
Erasmus K Masunga <115796851+eraskush@users.noreply.github.com>
2025-10-20 15:42:50 +0200

    header done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
   
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex ">
         
        <h1 >linkedin clone 2.0</h1>
           
---
+++ components/Header.tsx
@@ -0,0 +1,58 @@
+import { Briefcase, HomeIcon, MessageSquare, SearchIcon, UsersIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+function Header() {
+  return (
+    <div className="flex items-center p-2 max-w-6xl mx-auto ">
+        <Image 
+          src="/images/linkedin-logo.png" 
+          width={40}
+          height={40}
+          alt="linkedin Logo"   
+        />
+        <div className='flex-1 '>
+            <form className='flex items-center space-x-2 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96'>
+            <SearchIcon className='h-4 text-gray-600' />
+            <input 
+              type="text" 
+              placeholder="Search" 
+              className="bg-transparent flex-1 outline-none border-none"
+            />
+            </form>
+        </div>
+
+        <div className='flex items-center space-x-4 px-6'>
+            <Link href='/' className='icon hidden md:flex'>
+            <HomeIcon className='h-5' />
+            <p>Home</p>
+            </Link>
+
+            <Link href='/' className='icon hidden md:flex'>
+            <UsersIcon className='h-5' />
+            <p>Network</p>
+            </Link>
+
+            <Link href='/' className='icon hidden md:flex'>
+            <Briefcase className='h-5' />
+            <p>Jobs</p>
+            </Link>
+
+            <Link href='/' className='icon hidden md:flex'>
+            <MessageSquare className='h-5' />
+            <p>Messaging</p>
+            </Link>
+
+            {/* user button if signed in */}
+
+            {/*sign in button if not signed in  */}
+
+            
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
